645cce10402614e9400e3207b4314ca08ee35373
vanden-dummy <Vandspistol20@gmail.com>
2024-08-16 14:35:52 +0000

    got 3 char port

--- ports.sh
+++ ports.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-port=$(kubectl get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -c4-8)
+port=$(kubectl get service | grep frontend | tr -s ' ' | cut -d ' ' -f 5 | cut -d ':' -f 2 | cut -d '/' -f 1)
 echo "port is $port"
 
 ip=$(kubectl get nodes -o wide | head -n 2 | awk '{print $7}' | sed -n 2p)
